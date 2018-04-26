360f8f6ddfaaef2719a22eb83bd4a106bc344dc2
Jony Santos <jony.santos@bematech.com.br>
2018-04-26 20:14:47 -0300

    initial commit

--- setup/scripts/wait-until-mongodb4-secondary.started.sh
+++ setup/scripts/wait-until-mongodb4-secondary.started.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 # Wait for mongodb to elect a primary
 while [ "$RESULT" != "true" ]; do
-   RESULT=$(mongo --host mongodb4 --quiet --eval 'db.isMaster().issecondary');
+   RESULT=$(mongo --host mongodb4 --quiet --eval 'db.isMaster().secondary');
    echo "$(date) - Is mongodb4 secondary? $RESULT"
    sleep 5;
 done
