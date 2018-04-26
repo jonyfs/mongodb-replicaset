97cd183d6c6b74a07d7bf2782d43662d9ca17228
Jony Santos <jony.santos@bematech.com.br>
2018-04-26 20:46:47 -0300

    initial commit

--- setup/scripts/test.sh
+++ setup/scripts/test.sh
@@ -58,15 +58,17 @@ echo "$(date) - replicaset status:"
 mongo --host mongodb4:27017 --eval "rs.status()"
 echo "$(date) - inserting data..."
 mongo --host mongodb4:27017 insert-data.js
+
 echo "$(date) - creating new index ..."
 mongo --host mongodb4:27017 --eval "db.getCollection('contacts').createIndex({ name:1, email: 1},{sparse:true,background:true})"
+mongo --host mongodb4:27017 --eval "db.getCollection('contacts').stats().indexSizes"
 
 echo "$(date) - Starting mongodb1 testing..."
 echo "$(date) - replicaset status:"
 mongo --host mongodb1:27017 --eval "rs.status()"
 
 echo "$(date) - Adding mongodb4 to replicaset..."
-mongo --host mongodb1:27017 --eval "rs.add( { host: 'mongodbd4:27017', priority: 0.5 } );"
+mongo --host mongodb1:27017 --eval "rs.add( { host: 'mongodb4:27017', priority: 0.5 } );"
 sh wait-until-mongodb4-secondary.started.sh
 
 echo "$(date) - replicaset status:"
@@ -77,6 +79,12 @@ mongo --host mongodb1:27017 --eval "db.getCollection('contacts').stats().indexSi
 mongo --host mongodb2:27017 --eval "db.getCollection('contacts').stats().indexSizes"
 mongo --host mongodb3:27017 --eval "db.getCollection('contacts').stats().indexSizes"
 mongo --host mongodb4:27017 --eval "db.getCollection('contacts').stats().indexSizes"
+
+echo "$(date) - Count:"
+mongo --host mongodb1:27017 --eval "db.getCollection('contacts').count({})"
+mongo --host mongodb2:27017 --eval "db.getCollection('contacts').count({})"
+mongo --host mongodb3:27017 --eval "db.getCollection('contacts').count({})"
+mongo --host mongodb4:27017 --eval "db.getCollection('contacts').count({})"
 
 
 
